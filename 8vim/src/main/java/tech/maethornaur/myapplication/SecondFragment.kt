b319bb610f78006d700aa9014e49e793c266f03b
Pedro Mangabeira <MaethorNaur@users.noreply.github.com>
2023-07-16 18:51:39 +0100

    Update SecondFragment.kt

--- 8vim/src/main/java/tech/maethornaur/myapplication/SecondFragment.kt
+++ 8vim/src/main/java/tech/maethornaur/myapplication/SecondFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import tech.maethornaur.myapplication.databinding.FragmentSecondBinding
-
  
 class SecondFragment : Fragment() {
 
